e8968c1e489acf412a0849abaccdd40185809184
jibancat <mho026@ucr.edu>
2021-11-09 22:06:26 -0800

    slurm submit script, specifically for UCR HPCC

---
+++ slurm/submit_training.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# $1: training folder
+
+sbatch <<EOT
+#SBATCH --nodes=1
+#SBATCH --ntasks=8
+#SBATCH --mem=64G
+#SBATCH --job-name=hydro_emu_$1
+#SBATCH -p intel,batch
+#SBATCH --output="hydro_emu_$1.out"
+
+date
+
+cd /home/jibancat/Documents/PYTHON/emu_optimizer
+
+echo "----"
+# run python script
+~/.conda/envs/simrun/bin/python -c "from examples.make_validations import *;\
+do_validations(\
+folder='data/$1',\
+n_optimization_restarts=20,\
+n_fidelities=2,\
+turn_off_bias_nargp=False,\
+output_folder='output/$1')"
+
+hostname
+
+exit
+
+EOT
